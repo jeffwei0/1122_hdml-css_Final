08eee976e888f2c920c4d4f113fe910261083cd9
jeffwei0  <zxcv09210407zxcv@gmail.com>
2024-05-27 15:13:38 +0800

    編輯Final.html & design001.css & design001.js

--- design001.js
+++ design001.js
@@ -1,20 +1,41 @@
-function circle(){
-    let circleBtn = document.querySelector(".circleBtn");
-    container.classList.toggle("circle")
+function createBlocks(num) {
+    const container = document.querySelector(".container");
+    container.innerHTML = ''; // 清空現有的 blocks 和 buttons
+    container.innerHTML = `
+        <button onclick="generate();">Generate</button>
+        <button onclick="circle();generate();" class="circleBtn">Circle | Square</button>
+    `;
+    
+    const colors = ['white', 'lightblue', 'darkgrey'];
+    
+    for (let i = 0; i < num; i++) {
+        let block = document.createElement('div');
+        block.classList.add('block');
+        block.classList.add(colors[i % colors.length]); // 分配顏色
+        container.appendChild(block);
+    }
 }
-function generate(){
+
+function circle() {
+    let container = document.querySelector(".container");
+    container.classList.toggle("circle");
+}
+
+function generate() {
     anime({
         targets: ".block",
-        translateX: function (){
+        translateX: function () {
             return anime.random(-700, 700);
         },
-        translateY: function (){
+        translateY: function () {
             return anime.random(-500, 500);
         },
-        scale: function (){
+        scale: function () {
             return anime.random(1, 5);
         },
     });
 }
 
+
+createBlocks(60);
 generate();
